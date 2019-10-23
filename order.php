9938b193654a524bbf39f9de0d7f83e19960de62
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-10-22 22:50:46 -0400

    Add order page, first row (topping)
    added pepperoni topping support

--- order.php
+++ order.php
@@ -9,7 +9,26 @@
         <link rel="author" href="humans.txt">
     </head>
     <body>
-        
+        <div id="header">
+            <h1 align="center"> [Untitled]'s Pizza Online </h1>
+        </div>
+        <form>
+        <table>
+            <tr>
+                Meats:
+            </tr><tr>
+                <td>Pepperoni </td><td>
+                    <input type="radio" name="pepperoni" value="none"> None 
+                </td><td>
+                    <input type="radio" name="pepperoni" value="full"> Full 
+                </td><td>
+                    <input type="radio" name="pepperoni" value="left-half">Left only 
+                </td><td>
+                    <input type="radio" name="pepperoni" value="right-half">Right only 
+                </td>
+            </tr>
+
+        </table>
         
         <script src="js/main.js"></script>
     </body>
