c0e40cbdb1bb130957b033712fd80a16c2b1afcf
Jacob Hanchett <jjh132@zips.uakron.edu>
2019-10-23 15:51:20 -0400

    update nav links

--- nav.php
+++ nav.php
@@ -1,10 +1,10 @@
 <nav>
     <div class="home_link">
-    <a style="text-decoration: none;" href="index.html">
+    <a style="text-decoration: none;" href="index.php">
     
         <img src="img/untitled_logo.png">
         <h1>[Untitled]'s Pizza Online </h1>
     </a>
     </div>
-    <a style="margin: auto 10px auto 0;" href="sign_in.html">Sign in</a>
+    <a style="margin: auto 10px auto 0;" href="sign_in.php">Sign in</a>
 </nav>
