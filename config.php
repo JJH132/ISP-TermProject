2affebae1e326487b5ecb910761b6cf71c340877
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-10-31 12:43:06 -0400

    Add config.php file
    database connection file

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+define('DB_SERVER', 'aa148fg0lsw80en.cnb1cjruxnbc.us-east-2.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'administrator');
+define('DB_NAME', 'untitledpizza')
+
+
+$conn = mysqli_connect(DB_USERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if (!$conn) {
+    echo "connection failed";
+}
+echo "Connected successfully";
+?>
