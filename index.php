86f8ba400e0c06b80a1b7de288cec0b1b286b2c5
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-10-31 12:47:29 -0400

    change location of include

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
     <?php include 'htmlHead.php';?>
 </head>
 <body>
+    <?php include 'config.php' ?>
+
     <?php include 'nav.php' ?>
     <h1 style="color: #3D3D3D;">Untitled Pizza Home</h1>
 
     <a href="order.php">ORDER LINK</a>
-    <br>
-    <?php include 'config.php' ?>
-    <br>
+
 
     <br>
     <span style="font-size: 2.5em; margin: auto;">DEALS! DEALS! DEALS!</span>
