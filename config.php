3f5cbca463264bae787235e4ce5d9234e22676de
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-10-31 12:51:13 -0400

    add missing semicolon
    hopefully fix the issue

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'aa148fg0lsw80en.cnb1cjruxnbc.us-east-2.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'administrator');
-define('DB_NAME', 'untitledpizza')
+define('DB_NAME', 'untitledpizza');
 
 
 $conn = mysqli_connect(DB_USERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
