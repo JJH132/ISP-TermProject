b2bc3c26f9056538ba471334f04be853c96ad453
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-11-04 13:25:40 -0500

    Add signup page

--- config.php
+++ config.php
@@ -1,10 +1,4 @@
 <?php 
-define('DB_SERVER', 'aa148fg0lsw80en.cnb1cjruxnbc.us-east-2.rds.amazonaws.com');
-define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'administrator');
-define('DB_NAME', 'untitledpizza');
-
-
 $link = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
 // Check connection
 if (!$link) {
