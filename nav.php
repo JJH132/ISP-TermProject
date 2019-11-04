6bb411506d7a6ef118bc04c0aa4184dddf9a4c5b
Jacob Hanchett <jjh132@zips.uakron.edu>
2019-11-04 12:47:36 -0500

    adding template

--- nav.php
+++ nav.php
@@ -3,7 +3,7 @@
     <a style="text-decoration: none;" href="index.php">
     
         <img style="height: 100px" src="img/untitled_logo.png">
-        <h1><span style="color: #3D3D3D; font-size: 1.3em;">[</span>Untitled<span style="color: #3D3D3D; font-size: 1.3em;">]</span>'s Pizza Online </h1>
+        <h1><span style="color: #3D3D3D; font-size: 1.3em;">[</span>Untitled<span style="color: #3D3D3D; font-size: 1.3em;">]</span> Pizza Online </h1>
     </a>
     </div>
     <a class="rightNavLinks" href="sign_in.php">Sign in</a>
