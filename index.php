d6b0da8dc1223b2e603239fea9e4e6e8cfaa3649
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-11-12 12:24:08 -0500

    Add second attempt at user login/register

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['email'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['email']);
+    header("location: login.php");
+  }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,6 +24,25 @@
 
     <?php include 'nav.php' ?>
     <h1 style="color: #3D3D3D;">Untitled Pizza Home</h1>
+<div class="content">
+    <!-- notification message -->
+    <?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+        <h3>
+          <?php 
+            echo $_SESSION['success']; 
+            unset($_SESSION['success']);
+          ?>
+        </h3>
+      </div>
+    <?php endif ?>
+
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['email'])) : ?>
+        <p>Welcome <strong><?php echo $_SESSION['email']; ?></strong></p>
+        <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
+</div>
 
     <a href="order.php">ORDER LINK</a>
 
