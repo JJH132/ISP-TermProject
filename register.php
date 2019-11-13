88dd4999af98c5427cbdae046330310d3586baa5
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-11-13 15:46:44 -0500

    Change form location

--- register.php
+++ register.php
@@ -2,8 +2,6 @@
 <?php 
   //include the config file
 
-
-
   $link = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
   // Check connection
   if (!$link) {
