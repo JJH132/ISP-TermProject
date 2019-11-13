31f43a99354bae4220adfe3b6ab42c768e58a137
Jacob Hanchett <jjh132@zips.uakron.edu>
2019-11-13 11:13:45 -0500

    uncomment aws config

--- config.php
+++ config.php
@@ -18,10 +18,10 @@ set_exception_handler( 'handleException' );
 ?>
 
 <?php 
-// $link = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
-// // Check connection
-// if (!$link) {
-//     echo "connection failed";
-// }
-// echo "Connected successfully";
+$link = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
+// Check connection
+if (!$link) {
+    echo "connection failed";
+}
+echo "Connected successfully";
 ?>
