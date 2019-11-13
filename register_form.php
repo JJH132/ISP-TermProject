aa8cbec9914efc0d925165f505a04b084fb09800
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-11-13 15:13:03 -0500

    Add connection test

--- register_form.php
+++ register_form.php
@@ -1,5 +1,6 @@
 <?php 
-
+    $conn = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
+    
     $email = $_POST["email"];
     $password = $_POST["password"];
     $confirm_password = $_POST["confirm_password"];
