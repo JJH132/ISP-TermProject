94a806ddec644f115e9cad11b223621a1b49de80
Joseph Cochran <nwcochran11@gmail.com>
2019-11-13 13:30:07 -0500

    Start php to order page

--- order.php
+++ order.php
@@ -1,3 +1,12 @@
+<?php
+    $halfOrFull = $_GET["onoffswitch"];
+    $order = "";
+    if($halfOrFull == "HALF"){
+        $order .= "HALF: ";
+        
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -19,15 +28,15 @@
     <body>
         <?php include 'nav.php';?>
         <div align="center">
-            Toppings for full pizza or half/half?
-        <div class="onoffswitch" align="left">
-            <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked onclick="displaySecondTable()">
-            <label class="onoffswitch-label" for="myonoffswitch">
-                <span class="onoffswitch-inner"></span>
-                <span class="onoffswitch-switch"></span>
-            </label>
+                Toppings for full pizza or half/half?
+            <div class="onoffswitch" align="left">
+                <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked onclick="displaySecondTable()">
+                <label class="onoffswitch-label" for="myonoffswitch">
+                    <span class="onoffswitch-inner"></span>
+                    <span class="onoffswitch-switch"></span>
+                </label>
+            </div>
         </div>
-    </div>
         <br>
         <form>
         <table class="inlineTable" id="table1">
@@ -175,11 +184,11 @@
                 <td>
                 <div class="toppingH">Cheese:</div> 
                 </td><td>
-                    <input type="radio" name="cheese" value="normal" checked> Normal
+                    <input type="radio" name="cheese2" value="normal" checked> Normal
                 </td><td>
-                    <input type="radio" name="cheese" value="none" > No Cheese
+                    <input type="radio" name="cheese2" value="none" > No Cheese
                 </td><td>
-                    <input type="radio" name="cheese" value="extra-whole"> Extra Cheese
+                    <input type="radio" name="cheese2" value="extra-whole"> Extra Cheese
                 </td>
             </tr><tr>
             </tr><tr>
@@ -189,126 +198,127 @@
                     <div class="topping">Pepperoni</div>
                 </td><td>
                     <br><br><br><br>
-                    <input type="radio" name="pepperoni" value="none" checked> None 
+                    <input type="radio" name="pepperoni2" value="none" checked> None 
                 </td><td>
                     <br><br><br><br>
-                    <input type="radio" name="pepperoni" value="full"> Full 
+                    <input type="radio" name="pepperoni2" value="full"> Full 
                 </td>
             </tr><tr>
                 <td><div class="topping">Ham</div> </td><td>
-                    <input type="radio" name="ham" value="none" checked> None 
+                    <input type="radio" name="ham2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="ham" value="full"> Full 
+                    <input type="radio" name="ham2" value="full"> Full 
                 </td>
             </tr><tr>
                 <td><div class="topping">Bacon</div> </td><td>
-                    <input type="radio" name="bacon" value="none" checked> None 
+                    <input type="radio" name="bacon2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="bacon" value="full"> Full 
+                    <input type="radio" name="bacon2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td><div class="topping">Sausage</div> </td><td>
-                    <input type="radio" name="sausage" value="none" checked> None 
+                    <input type="radio" name="sausage2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="sausage" value="full"> Full 
+                    <input type="radio" name="sausage2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td> <div class="topping">Salami </div> </td><td>
-                    <input type="radio" name="salami" value="none" checked> None 
+                    <input type="radio" name="salami2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="salami" value="full"> Full 
+                    <input type="radio" name="salami2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td> <div class="topping">Beef </div></td><td>
-                    <input type="radio" name="beef" value="none" checked> None 
+                    <input type="radio" name="beef2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="beef" value="full"> Full 
+                    <input type="radio" name="beef2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td> <div class="topping">Anchovies </div> </td><td>
-                    <input type="radio" name="anchovies" value="none" checked> None 
+                    <input type="radio" name="anchovies2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="anchovies" value="full"> Full 
+                    <input type="radio" name="anchovies2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td> <div class="topping">Chicken </div> </td><td>
-                    <input type="radio" name="chicken" value="none" checked> None 
+                    <input type="radio" name="chicken2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="chicken" value="full"> Full 
+                    <input type="radio" name="chicken2" value="full"> Full 
                 </td>
             </tr><tr>
 
                  <td> <div class="topping">Pork </div> </td><td>
-                    <input type="radio" name="pork" value="none" checked> None 
+                    <input type="radio" name="pork2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="pork" value="full"> Full 
+                    <input type="radio" name="pork2" value="full"> Full 
                 </td>
             </tr><tr>
                 <td> <br><br> <div class="toppingH">Non-Meat Options: </div> </td>
             </tr><tr>
                  <td> <div class="topping">Pineapple </div> </td><td>
-                    <input type="radio" name="pineapple" value="none" checked> None 
+                    <input type="radio" name="pineapple2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="pineapple" value="full"> Full 
+                    <input type="radio" name="pineapple2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td> <div class="topping">Mushrooms </div> </td><td>
-                    <input type="radio" name="mushrooms" value="none" checked> None 
+                    <input type="radio" name="mushrooms2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="mushrooms" value="full"> Full 
+                    <input type="radio" name="mushrooms2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td> <div class="topping">Onions </div> </td><td>
-                    <input type="radio" name="onions" value="none" checked> None 
+                    <input type="radio" name="onions2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="onions" value="full"> Full 
+                    <input type="radio" name="onions2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td> <div class="topping">Hot Sauce </div> </td><td>
-                    <input type="radio" name="hot-sauce" value="none" checked> None 
+                    <input type="radio" name="hot-sauce2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="hot-sauce" value="full"> Full 
+                    <input type="radio" name="hot-sauce2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td> <div class="topping">Spinach </div> </td><td>
-                    <input type="radio" name="spinach" value="none" checked> None 
+                    <input type="radio" name="spinach2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="spinach" value="full"> Full 
+                    <input type="radio" name="spinach2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td> <div class="topping">Diced Tomatoes </div> </td><td>
-                    <input type="radio" name="tomatoes" value="none" checked> None 
+                    <input type="radio" name="tomatoes2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="tomatoes" value="full"> Full 
+                    <input type="radio" name="tomatoes2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td> <div class="topping">Olives </div> </td><td>
-                    <input type="radio" name="olives" value="none" checked> None 
+                    <input type="radio" name="olives2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="olives" value="full"> Full 
+                    <input type="radio" name="olives2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td> <div class="topping">Banana Peppers </div></td><td>
-                    <input type="radio" name="banana-peppers" value="none" checked> None 
+                    <input type="radio" name="banana-peppers2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="banana-peppers" value="full"> Full 
+                    <input type="radio" name="banana-peppers2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td><div class="topping">Red Peppers </div> </td><td>
-                    <input type="radio" name="red-peppers" value="none" checked> None 
+                    <input type="radio" name="red-peppers2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="red-peppers" value="full"> Full 
+                    <input type="radio" name="red-peppers2" value="full"> Full 
                 </td>
             </tr><tr>
                  <td> <div class="topping">Jalapeño Peppers </div> </td><td>
-                    <input type="radio" name="jalapeno-peppers" value="none" checked> None 
+                    <input type="radio" name="jalapeno-peppers2" value="none" checked> None 
                 </td><td>
-                    <input type="radio" name="jalapeno-peppers" value="full"> Full 
+                    <input type="radio" name="jalapeno-peppers2" value="full"> Full 
                 </td>
             </tr><tr>
             </tr>
         </table>
-
+        <br>
+        <input type="submit" value="submit">
         <?php include 'footer.php' ?>
         
         <script src="js/main.js"></script>
