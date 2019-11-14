79cb81fde6862f1f2a92f38f7422204b264a9394
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-11-13 20:15:22 -0500

    Simplify Login page

--- loginuser.php
+++ loginuser.php
@@ -2,27 +2,29 @@
 
     $conn = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
 
-	if(isset($_POST['sign_in_button']))
+	if(isset($_POST['txt_email']) && isset($_POST['txt_pass']))
 	{
 		$email = mysqli_real_escape_string($conn, $_POST['txt_email']);
 		$password = mysqli_real_escape_string($conn, $_POST['txt_pass']);
 
 		if($email != "" && $password != "")
 		{
-			$sql = "SELECT count(*) AS usrcnt FROM ebdb.users WHERE email='".$email."' AND password='".$password."'";
-			$rs = mysqli_query($conn, $sql);
+			$sql = "SELECT * FROM ebdb.users WHERE email='$email' AND password='$password'";
+
+			$rs = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+			$count = mysqli_num_rows($rs);
 			$r = mysqli_fetch_array($rs);
 
-			$count = $row['usrcnt'];
+			
 
-			if($count > 0)
+			if($count == 1)
 			{
 				$_SESSION['email'] = $email;
-				header('Location: welcome.php');
+				echo "<script type='text/javascript'>alert('Login Credentials verified')</script>";
 			}
 			else
 			{
-				echo "Invalid email and password";
+				echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
 			}
 		}
 
--- sign_in.php
+++ sign_in.php
@@ -8,20 +8,20 @@
     <body>
         <?php include 'nav.php';?>
     	<div align="center" id="sign_in">
-            <form action="" method="post">
+            <form action="loginuser.php" method="post">
                 <table id="Sign_in_table" align="center">
                 	<tr>
                 		<td align="center"><h3>Existing user? Sign in!</h3></td>
                 	</tr>
                 	<tr>
-                		<td><b>Email:</b> <input type="text" name="txt_email"></td>
+                		<td><b>Email:</b> <input type="text" name="txt_email" id="txt_email"></td>
                 	</tr>
                 	<tr>
-                		<td><b>Password:</b> <input type="Password" name="txt_pass"></td>
+                		<td><b>Password:</b> <input type="Password" name="txt_pass" id="txt_pass"></td>
                 	</tr>
                 </table>
             </form>
-        	<button type="submit" id="sign_in_button" name="sign_in_button" align="center">Sign In</button>
+        	<button type="submit" id="sign_in_button" name="sign_in_button">Sign In</button>
         </div>
         <span id="sign_up">
 
