4e8c77f4f998404455b706ba08076d89242f17e7
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-11-18 18:26:47 -0500

    Add other links to nav.php

--- nav.php
+++ nav.php
@@ -8,6 +8,8 @@
     <?php 
     session_start();
     	echo '<a class="rightNavLinks" href="order.php">Order </a>';
+    	echo '<a class="rightNavLinks" href="aboutus.php">About Us</a>';
+    	echo '<a class="rightNavLinks" href="contactus.php">Contact Us</a>';
     if(isset($_SESSION['email']))
 	    {
 	    	echo "Hello " . $_SESSION['email'] . ".<br>";
