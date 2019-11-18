26c3e63d9b01f63104814d5e1b3612534af3366f
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-11-18 17:36:07 -0500

    Add logout.php and link to logout

--- nav.php
+++ nav.php
@@ -1,21 +1,21 @@
 <nav>
     <div class="home_link">
     <a style="text-decoration: none;" href="index.php">
-    
-        <img style="height: 100px" src="img/untitled_logo.png">
-        <h1><span style="color: #3D3D3D; font-size: 1.3em;">[</span>Untitled<span style="color: #3D3D3D; font-size: 1.3em;">]</span> Pizza Online </h1>
+    <img style="height: 100px" src="img/untitled_logo.png">
+    <h1><span style="color: #3D3D3D; font-size: 1.3em;">[</span>Untitled<span style="color: #3D3D3D; font-size: 1.3em;">]</span> Pizza Online </h1>
     </a>
     </div>
     <?php 
     session_start();
     if(isset($_SESSION['email']))
-    {
-    	echo "Hello " . $_SESSION['email'] . ".<br>";;
-    }
-    else
-    {
-    	echo '<a class="rightNavLinks" href="sign_in.php">Sign in</a>';
+	    {
+	    	echo "Hello " . $_SESSION['email'] . ".<br>";;
+	    	echo '<a class="rightNavLinks" href="logout.php">Log out </a>';
+	    }
+	    else
+	    {
+	    	echo '<a class="rightNavLinks" href="sign_in.php">Sign in</a>';
 
-    }
+	    }
     ?>
 </nav>
