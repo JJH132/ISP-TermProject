7dada9264c8c2056e134c7f392d1b800fe5d3e2a
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-11-18 19:58:41 -0500

    Add accountdetails.php and added it to nav bar

--- nav.php
+++ nav.php
@@ -10,6 +10,7 @@
     	echo '<a class="rightNavLinks" href="order.php">Order </a>';
     	echo '<a class="rightNavLinks" href="aboutus.php">About Us</a>';
     	echo '<a class="rightNavLinks" href="contactus.php">Contact Us</a>';
+    	echo '<a class="rightNavLinks" href="accountdetails.php">Account Details</a>';
     if(isset($_SESSION['email']))
 	    {
 	    	echo "Hello " . $_SESSION['email'] . ".<br>";
